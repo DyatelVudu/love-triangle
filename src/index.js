90f911da332f46bbf35ab218acabef3561347fd3
Ilya Kutko <36884482+DyatelVudu@users.noreply.github.com>
2018-09-14 08:39:18 +0300

    Added Love Triangle function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    // your implementation
+    var counter = 0;
+    var num = 0;
+    if (preferences.length > 2) {
+      for (var i = 0; i < preferences.length; i++) {
+        num = preferences[i];
+        if (num == (i + 1)) continue;
+        num = preferences[preferences[num - 1] - 1];
+        if (num == (i + 1)) {
+          count++;
+        }
+      }
+      return counter/3; 
+    } else {
+      return counter;
+    }
+}  
